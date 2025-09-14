352b55e78cddd262117c6108dd1c8ff3017f6748
Eesha Hasini <eesha.hasini.007@gmail.com>
2025-09-14 21:25:07 +0530

    final commits done

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>ZOMATO | CONTACT US : PHONE-79895XXXXX</p>
+    </footer>
+  );
+}
+export default Footer;
